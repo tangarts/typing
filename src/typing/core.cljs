9b037e3edf1f7a0edbb5b54ddd523b18216a8fcd
tangarts <nehemiahcampbell@hotmail.co.uk>
2021-01-15 20:25:04 +0000

    refactoring and input anywhere you click TODO: * cursor available on first character - More refactoring - styling - menu - stats

--- src/typing/core.cljs
+++ src/typing/core.cljs
@@ -27,58 +27,26 @@
     (= a b) "current"
     :else "awaiting"))
 
-(def typing-state
-  #{"not-started"
-    "in-prog"
-    "awaiting"
-    "finished"})
-
-
-(def cpm (r/atom 0))
-
-(defonce app-state (r/atom
-             {:i 0
-              :typing-state
-              (typing-state "not-started")}))
-
-
-; characters is wrong, doesnt include spaces
-; characters should handle text better
-(defn get-cpm
-  [input-words]
-  (let [time-elapsed (@app-state :time)
-        characters (reduce + (map count input-words))]
-    (/ (Math/floor (* characters 60)) time-elapsed)))
-
-
 (defn get-app-element []
   (gdom/getElement "app"))
-
-
 
 (defn words [expected actual]
   (let [idx (range (count expected))
         variant (for [i (range (count expected))]
-                  (get-word-type i (count @actual)))
-        ] 
+                  (get-word-type i (count @actual)))] 
     (into [:div ]
-          (map #(word (nth expected %2) 
-                      (get @actual %2) %1) variant idx))))
+          (map #(word (nth expected %1)
+                      (get @actual %1) %2) idx variant))))
 
-
-; (map #(get-word-type % (dec (count expected))) idx)
 
 (def text
   (str/split
-    "In today's technology landscape, the web is king.
-    The JavaScript community is also one of the most active and prolific software development
-    communities ever, with libraries and frameworks for any conceivable use." #"")) 
+    "In today's technology landscape, the web is king. The JavaScript community is also one of the most active and prolific software development communities ever, with libraries and frameworks for any conceivable use." #"")) 
 
 
 
 (comment 
   (for [i (range (count text))] (get-word-type i (count @input-ref)))
-
   (doseq
     [[i word] (map-indexed vector text)]
     [:span {:class ""
@@ -88,101 +56,54 @@
 
 )
 
-(defn check-correct [input! text i]
-  (when (<= (count @input!) i) 
-    (= (last @input!) (nth text i))))
-
-; render text first and only update class on input :)
-
-(defn render-text [text input!]
-  (let [style-class (r/atom "")]
-    (fn [text input!]
-    (into [:div#text ]
-          (for [[i character] (map-indexed vector text)]
-            [:span {:class (if (check-correct input! text i) "correct" "")
-                    :style word-css} character])))))
-
-
-; (if (check-correct input! text) "correct" "incorrect")
-
-; input and text body
-; incorrect: input and substring of text not equal in length to input are equal
-; corrent input and substring of text equal in length to input are equal 
-; current length on input
-
-
 ; (fn [] (.addEventListener js/document "input" handler))
 
 ; (def did-mount {:component-did-mount #(.focus (rdom/dom-node %)))
 
+(defn initial-focus-wrapper [element]
+  (with-meta element
+    {:component-did-mount #(.focus (rdom/dom-node %))}))
+
 (defn text-area [input-ref!]
-  [:input#input {
+  (fn []
+    [:input#input {
            ; :style {:position "fixed" :left "-100%" :top "50%"} ; hidden
            :type "text"
-           :margin "none"
-           :full-width "true"
-           :variant "outlined"
+           :auto-focus "true"
            :default-value @input-ref!
-           :on-change #(reset! input-ref! (-> % .-target .-value))}])
+           :on-change #(reset! input-ref! (-> % .-target .-value))}]))
 
-(def input-ref! (r/atom nil))
+(defn focus []
+  (-> js/document (.getElementById "input") (.focus)))
 
 (defn app [] 
-  (let [;input-ref! (r/atom nil)
-        words-ref! (r/atom nil)
-        current (nth @input-ref! (:i @app-state))]
+  (let [input-ref! (r/atom nil)
+        words-ref! (r/atom nil) ]
+  
     (fn []
-      [:div {:style style/root}
-       [:div {:style style/container}
+      [:div {:style style/root :on-click #(focus)}
+       [:div {:style style/container} 
         [paper {:elevation "4" :style style/board}
          [paper {:elevation "2"}
           [:div {:style style/statistics}
            [:div "timer: " ; FIX 
-            (if (= (@app-state :typing-state) "awaiting") "finsh word")
+            
+             ]
+           [:div {:style {:margin-left "20"}}
+            "CPM: " ; Fix
             ]
            [:div {:style
-                  {:margin-left "20"}}
-            "CPM: " (if (or (= @cpm nil) (= @cpm Math/Inf)) "?" @cpm)]
-           [:div {:style
                   {:flex-grow "1"}}]
-           [icon-button {:color "inherit" 
-                         :onclick " reset fn"}
-            [:i {:class "material-icons"} "replay" ]]]]
+           [:a {:color "inherit"
+                :onclick " reset fn"}
+            "replay"]]]
          [:div {:style {:padding "35px 15px"}}
 
           [:div {:style style/inputs}
-
-           [render-text text input-ref!]
-           [:div @input-ref!]
+           [text-area input-ref!] ; text-area
            [words text (r/atom (str/split @input-ref! #""))]
-           ]]]
-        [paper {:style {:padding "0px"}}
-         ; text-area
-         [text-area input-ref!]
-         ]]
-       ]
+           ]]]]]
       )))
-
-
-(comment
-
-  [:div.timer#timer]
-  [:div.container
-   [:div.quote-display#quote-display]
-   [:textarea.quote-input#quote-input]
-   ]
-  quote-display-element.event-listener('input',)
-  quote-input-element
-  timer-element
-
-
-
-(def actual "if by she go give you enve ou ")
-(def expected "if by she go give you even our")
-
-
-
-)
 
 
 (defn results-dialog []
@@ -191,7 +112,6 @@
     :expected "words todo"
     :actual "input-words todo"
     :close-dialog "close-dialog"}])
-
 
 
 (defn mount [el]
