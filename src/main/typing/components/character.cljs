a1f68fe01a9a12f8178b71427502ee89376947eb
Nehemiah Campbell <nehemiahcampbell@hotmail.co.uk>
2021-11-06 16:02:12 +0000

    clean up character file

--- src/main/typing/components/character.cljs
+++ src/main/typing/components/character.cljs
@@ -3,23 +3,27 @@
     [typing.components.style :refer [word-css]]))
 
 (defn character
+  "
+  A character can be:
+    - waiting '' (or (nil? actual) (<= (count actual) i))
+    - done -> {correct,incorrect}
+    - current (= i (count @input-ref!))
+    
+    update class with atoms ['' 'done' 'current']
+  "
   [expected actual variant]
   (let [is-match (= expected actual)]
+    ; :class form is a vector? should it be?
     [:span {:style word-css
-            :class
-            [(case variant
-               "done" (if is-match "correct" "incorrect")
-               "current" "cursor"
-               "awaiting")]}
-     expected]))
+            :class [(case variant
+                      "done" (if is-match "correct" "incorrect")
+                      "current" "cursor"
+                      "awaiting")]} expected]))
 
+(defn get-character-state [a b]
+  (cond 
+    (< a b) "done"
+    (= a b) "current"
+    :else "awaiting"))
 
-(comment 
-  ; alternative to case
-
-; (cond (= variant "done")
-;                    (if is-match "correct" "incorrect")
-;                    (= variant "current") "cursor"
-;                    :else "awaiting") 
-  )
 
